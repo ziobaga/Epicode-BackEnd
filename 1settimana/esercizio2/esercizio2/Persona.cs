464e59e3f3b1c8033f18ccc5a0add424961837a4
ziobaga <adrianoleanza01@gmail.com>
2024-06-18 14:57:07 +0200

    okk

---
+++ 1settimana/esercizio2/esercizio2/Persona.cs
@@ -0,0 +1,34 @@
+﻿internal class Persona
+{
+    public string _nome;
+    public string _cognome;
+    public int _eta;
+
+    public Persona (string nome, string cognome, int eta)
+    {
+        _nome = nome;
+        _cognome = cognome;
+        _eta = eta;
+    }
+
+    public string getNome()
+    {
+        return _nome;
+    }
+
+    public string getCognome()
+    {
+        return _cognome;
+    }
+
+    public int getEta()
+    {
+        return _eta;
+    }
+
+    public void getDettagli()
+    {
+        Console.WriteLine("Nome: " + _nome + " Cognome: " + _cognome + " Età: " + _eta);
+    }
+
+}
---
+++ 1settimana/esercizio2/esercizio2/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    public static void Main(string[]args)
+    {
+        Persona a = new Persona("Mario", "Rossi", 30);
+
+        Console.WriteLine(a.getNome());
+        Console.WriteLine(a.getCognome());
+        Console.WriteLine(a.getEta());
+        a.getDettagli();
+    }
+}
