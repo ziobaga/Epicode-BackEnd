1524a6bfe02a6e4d3f373b96144f793b3b77edd1
ziobaga <adrianoleanza01@gmail.com>
2024-06-19 17:14:30 +0200

    es3

---
+++ 1settimana/esercizio3/esercizio3(3)/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 0;
+        Console.WriteLine("Specifica dimensione array");
+        x = int.Parse(Console.ReadLine());
+        int[] listanumeri = new int[x];
+
+        int i;
+        int somma = 0;
+
+        for (i = 0; i < listanumeri.Length; i++)
+        {
+            Console.WriteLine("Inserisci numero");
+            listanumeri[i] = int.Parse(Console.ReadLine());
+        }
+        for (i = 0; i < listanumeri.Length; i++)
+        {
+            somma += listanumeri[i];
+        }
+        Console.WriteLine($"\nLa somma è: {somma}\n");
+        Console.WriteLine($"La media è: {somma / x}");
+        Console.ReadLine();
+    }
+}
