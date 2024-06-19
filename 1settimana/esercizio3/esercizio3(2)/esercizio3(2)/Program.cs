1a50c31b500ac7c375f287038cef94d73e34567e
ziobaga <adrianoleanza01@gmail.com>
2024-06-19 16:59:50 +0200

    secondo es

---
+++ 1settimana/esercizio3/esercizio3(2)/esercizio3(2)/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] nomi = { "Adriano", "Andrea", "Checco", "Flavio", "Stefano" };
+        Console.WriteLine("Cerca un nome:");
+        Cerca(nomi);
+    }
+    static void Cerca(string[] nomi)
+    {
+        string nome = Console.ReadLine();
+        bool check = false;
+
+        for (int i = 0; i < nomi.Length; i++)
+        {
+            if (nome == nomi[i])
+            {
+                check = true;
+                Console.WriteLine("Il nome esiste");
+            }
+        }
+
+        if (!check)
+        {
+            Console.WriteLine("Il nome non esiste. Riprova.");
+            Cerca(nomi);
+        }
+    }
+}
