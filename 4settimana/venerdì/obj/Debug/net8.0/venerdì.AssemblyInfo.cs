1fe52542c1cb079b1f6172a03f5346325b67579e
ziobaga <adrianoleanza01@gmail.com>
2024-07-19 16:55:51 +0200

    fine 2

--- 4settimana/venerdì/obj/Debug/net8.0/venerdì.AssemblyInfo.cs
+++ 4settimana/venerdì/obj/Debug/net8.0/venerdì.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("venerdì")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551613bba81f213a9bd53ad62e6a3ee9ce7389e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad4968d6dd646e2814c66f488714ea3f04e4630")]
 [assembly: System.Reflection.AssemblyProductAttribute("venerdì")]
 [assembly: System.Reflection.AssemblyTitleAttribute("venerdì")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
